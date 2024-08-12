14a5a63dcb145ec57ab2d4c95ef2dd8dfe73b67d
nickdev <nick@nickhughes.io>
2024-08-12 11:18:03 +1000

    initialise database

--- project2/project.py
+++ project2/project.py
@@ -1,12 +1,37 @@
 import sqlite3
+import streamlit as st
 from sqlite3 import Error
 
 
 def main():
-    create_connection("contacts.db")
+#    create_connection("contacts.db")
+    conn = sqlite3.connect('work_contacts.db')
+
+    # Initialise Database
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS employees 
+                   (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, company TEXT, position TEXT, email TEXT, phone REAL)''')
+    conn.commit()
 
 
 
+
+
+
+    if st.button("List"):
+        st.info("Display List")
+
+    if st.button("Create"):
+        st.info("Create Record")
+
+    if st.button("Read"):
+        st.info("Read Record")
+
+    if st.button("Update"):
+        st.info("Update Record")
+
+    if st.button("Delete"):
+        st.info("Delete Record")
 
 def create_connection(path):
     connection = None
