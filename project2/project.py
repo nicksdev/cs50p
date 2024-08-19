8df13ee272447077a1a937758d3b54665648734d
Nick Hughes <nick@nickhughes.io>
2024-08-19 12:28:08 +1000

    Capture input

--- project2/project.py
+++ project2/project.py
@@ -1,36 +1,28 @@
-#import sqlite3
+
 import streamlit as st
 import csv
-#from sqlite3 import Error
+
 
 
 def main():
+    firstname = st.text_input("Firstname", value="", max_chars=None, key=None)
+    lastname = st.text_input("Lastname", value="", max_chars=None, key=None)
+    company = st.text_input("Company", value="", max_chars=None, key=None)
+    phone = st.text_input("Phone", value="", max_chars=None, key=None)
+    email = st.text_input("Email", value="", max_chars=None, key=None)
+    
     with open('contacts.csv', 'w', newline='') as file:
-        firstname = "Nick"
-        lastname = "Hughes"
-        company = "Luvian Consulting"
-        phone = "0403 553977"
-        email = "nick@nickhughes.io"
+        # firstname = "Nick"
+        # lastname = "Hughes"
+        # company = "Luvian Consulting"
+        # phone = "0403 553977"
+        # email = "nick@nickhughes.io"
         writer = csv.writer(file)
         field = ["firstname", "lastname", "company", "phone", "email"]
         
         writer.writerow(field)
         writer.writerow([firstname, lastname, company, phone, email])
 
-#    create_connection("contacts.db")
-
-
-
-
-# def create_connection(path):
-#     connection = None
-#     try:
-#         connection = sqlite3.connect(path)
-#         print("Connection to SQLite DB successful")
-#     except Error as e:
-#         print(f"The error '{e}' occurred")
-
-#     return connection
 
 def create_contact():
     ...
