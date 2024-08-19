c5f841a4abb0565dd6b9c10f75e6fae7fc79b3b1
Nick Hughes <nick@nickhughes.io>
2024-08-19 10:29:19 +1000

    remove database

--- project2/project.py
+++ project2/project.py
@@ -1,53 +1,24 @@
-import sqlite3
+#import sqlite3
 import streamlit as st
-from sqlite3 import Error
+#from sqlite3 import Error
 
 
 def main():
+    ...
 #    create_connection("contacts.db")
-    conn = sqlite3.connect('work_contacts.db')
-
-    # Initialise Database
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS contacts 
-                   (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT, company TEXT, position TEXT, email TEXT, phone REAL)''')
-    conn.commit()
 
 
 
 
-    if st.button("Add Test Data"):
-        cursor.execute("INSERT INTO contacts (firstname, lastname, company, position, email, phone) VALUES (?, ?, ?, ?, ?, ?)", ('testfirst', 'testlast', 'testcompany', 'test CEO', 'test@test.com', '0403 123456)'))
-        conn.commit()
- 
-    if st.button("List"):
-        cursor.execute("SELECT * FROM contacts")
-        rows = cursor.fetchall()
-        for row in rows:
-        #print(row)
-            st.info(row)
+# def create_connection(path):
+#     connection = None
+#     try:
+#         connection = sqlite3.connect(path)
+#         print("Connection to SQLite DB successful")
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
 
-    if st.button("Create"):
-        st.info("Create Record")
-
-    if st.button("Read"):
-        st.info("Read Record")
-
-    if st.button("Update"):
-        st.info("Update Record")
-
-    if st.button("Delete"):
-        st.info("Delete Record")
-
-def create_connection(path):
-    connection = None
-    try:
-        connection = sqlite3.connect(path)
-        print("Connection to SQLite DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-
-    return connection
+#     return connection
 
 def create_contact():
     ...
