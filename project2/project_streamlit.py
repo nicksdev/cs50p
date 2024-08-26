91b0056b5dd66a3b54325efe2a648e1a1345663d
Nick Hughes <nick@nickhughes.io>
2024-08-26 10:12:43 +1000

    Flask experiment

--- project2/project_streamlit.py
+++ project2/project_streamlit.py
@@ -2,21 +2,43 @@
 import streamlit as st
 import csv
 
+
+
 class Contact:
     def __init__(self, firstname, lastname, company, phone, email):
+        #self.userid = userid
         self.firstname = firstname
         self.lastname = lastname
         self.company = company
         self.phone = phone
         self.email = email
 
+
+
+def initid():
+    if "userid" in globals():
+        print("found")
+        userid = userid + 1
+        return userid
+    else:
+        print("notfound")
+        
+        userid = 1
+        return userid
+
+
+
 def main():
+
+
+    
+
     contact = get_contact()
     if st.button('Create'):
+        #print(userid)
         #print(contact.firstname, contact.lastname, contact.company, contact.phone, contact.email)
         create_contact(contact)
-
-
+        
 
 
 def get_contact():
@@ -29,29 +51,12 @@ def get_contact():
     return contact
 
 
-
-
-
-
-
-    
-    # with open('contacts.csv', 'w', newline='') as file:
-    #     # firstname = "Nick"
-    #     # lastname = "Hughes"
-    #     # company = "Luvian Consulting"
-    #     # phone = "0403 553977"
-    #     # email = "nick@nickhughes.io"
-    #     writer = csv.writer(file)
-    #     field = ["firstname", "lastname", "company", "phone", "email"]
-        
-    #     writer.writerow(field)
-    #     writer.writerow([firstname, lastname, company, phone, email])
-
-
 def create_contact(contact):
-    with open('contacts.csv', 'w', newline='') as file:
+    with open('contacts.csv', 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([contact.firstname, contact.lastname, contact.company, contact.phone, contact.email])
+        writer.writerow([initid(), contact.firstname, contact.lastname, contact.company, contact.phone, contact.email])
+        
+
 
 
 def read_contact():
