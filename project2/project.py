acd15f8425aed287f14d4e88862ee1723e4ebf78
Nick Hughes <nick@nickhughes.io>
2024-08-26 13:21:55 +1000

    Introduce id

--- project2/project.py
+++ project2/project.py
@@ -1,6 +1,8 @@
 import sys
 import csv
 
+
+
 class Contact:
     def __init__(self, firstname, lastname, company, phone, email):
         #self.userid = userid
@@ -12,9 +14,10 @@ class Contact:
 
 
 def main():
+    contactid = init_id()
     input = sys.argv
     contact = get_contact(input)
-    crud_call(input,contact)
+    crud_call(input,contact,contactid)
     #print(contact.firstname)
     
 
@@ -25,23 +28,19 @@ def main():
 
 
 
-# def create_contact(contact):
-#     with open('contacts.csv', 'a', newline='') as file:
-#         writer = csv.writer(file)
-#         writer.writerow([initid(), contact.firstname, contact.lastname, contact.company, contact.phone, contact.email])
-
-def crud_call(input,contact):
+def crud_call(input,contact,contactid):
 
     if input[1] == "create":
-        create_contact(contact)
+        create_contact(contact,contactid)
     else:
         print("Unrecognised Input")
 
-def create_contact(contact):
+def create_contact(contact,contactid):
     print(contact.email)
+    id = get_contactid(contactid)
     with open('contacts.csv', 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([contact.firstname, contact.lastname, contact.company, contact.phone, contact.email])
+        writer.writerow([id, contact.firstname, contact.lastname, contact.company, contact.phone, contact.email])
     
 
 def read_contact():
@@ -61,6 +60,26 @@ def get_contact(input):
     email = input[6]
     contact = Contact(firstname, lastname, company, phone, email)
     return contact
+
+def get_contactid(contactid):
+    print(contactid)
+    contactid = contactid + 1
+    print(contactid)
+    return contactid
+
+def init_id():
+    print("Initialising ID")
+    try:
+        contactid = contactid + 1
+        return contactid
+        
+    
+    except NameError:
+        print("Setting ID to ZERO")
+        contactid = 0
+        return contactid
+
+
 
 if __name__ == "__main__":
     main()
